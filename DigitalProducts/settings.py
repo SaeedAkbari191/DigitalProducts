c7db1673f70c57dc06d158512d73a7dd415eab48
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-10-07 22:33:01 +0430

    product Models

--- DigitalProducts/settings.py
+++ DigitalProducts/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 
 # MEDIA FILES
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
