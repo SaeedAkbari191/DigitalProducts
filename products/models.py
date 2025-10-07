07a9ff5648853f5ed1fc3988aaf6eec915a5d37a
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-10-07 22:14:58 +0430

    products app addes

--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="Title", max_length=100, )
+    description = models.TextField(verbose_name="Description", blank=True)
+    avatar = models.ImageField(verbose_name="Avatar", blank=True, upload_to="Categories/")
+    is_enabled = models.BooleanField(verbose_name='Is Enabled', default=True)
+    created_at = models.DateTimeField(verbose_name='Created at', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Update At', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        db_table = 'Category'
+
+
+class Product(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=50)
+    description = models.TextField(verbose_name="Description", blank=True)
+    avatar = models.ImageField(verbose_name="Avatar", blank=True, upload_to="Products/")
+    is_enabled = models.BooleanField(verbose_name='Is Enabled', default=True)
+    category = models.ManyToManyField(Category, verbose_name="Category", blank=True)
+    created_at = models.DateTimeField(verbose_name='Created at', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Update At', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        db_table = 'Product'
+
+
+class File(models.Model):
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="Title", max_length=50)
+    file = models.FileField(verbose_name="File", upload_to="Files/%Y/%m/%d/")
+    is_enabled = models.BooleanField(verbose_name='Is Enabled', default=True)
+    created_at = models.DateTimeField(verbose_name='Created at', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Update At', auto_now=True)
+
+    class Meta:
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
+        db_table = 'File'
